a4851cd55e0da556ef3215b2417b545884056c9b
Jithin Sabu <jithinsabu@Jithins-MacBook-Pro-2.local>
2025-02-01 15:27:32 +0530

    Updated site

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,22 +1,29 @@
 import * as React from "react";
-import hero from './assets/hero-image.svg';
-
+import herodesktop from './assets/hero-image.svg';
+import heromobile from './assets/hero-image-mobile.svg';
 export function Hero() {
   return (<div className="hero-container">
   <div className="hero-container-text">
             <div className="hero-text">
-                <h1>
+                <h1 className="break-desktop">
                 Because life happens <br /> between that 2 hour video...
                 </h1>
-                <h2>
+                <h1 className="break-mobile">
+                Because life <br />happens between <br />that 2 hour video... 
+                </h1>
+                <h2 className="break-desktop-h2">
                 Save timestamps in long videos and pick up exactly where you left off. <br /> Like a bookmark for your brain, but for YouTube tutorials.
+                </h2>
+                <h2 className="break-mobile-h2">
+                Save timestamps in long videos and pick up exactly where you left off.  Like a bookmark <br />for your brain, but for YouTube tutorials.
                 </h2>
             </div>
                 <div className="download-btn">
                 Download extension - it's free
                 </div>
         </div>
-        <img src={hero} alt="Hero Image" className="hero-image" />
+        <img src={herodesktop} alt="Hero Image" className="hero-image-desktop" />
+        <img src={heromobile} alt="Hero Image" className="hero-image-mobile" />
         </div>
   );
 }
